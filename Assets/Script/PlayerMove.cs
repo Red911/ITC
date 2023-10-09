a52009db4146736d1c53e7c64a369f346570f6f1
Romain <mromain911@gmail.com>
2023-10-09 11:23:13 +0200

    Virtual Cam

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform endingPoint;
+    
+    
+
+    
+}
