d59d4b14f51793aed8a4e78d78328a8ee4a297fc
AkuroP <thomas.kang2002@gmail.com>
2023-10-09 11:33:40 +0200

    Audio Manager Imported
    + Audio Manager (Service Locator)

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,13 +1,20 @@
+using Game.Script.SoundManager;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform endingPoint;
-    
-    
 
-    
+    [SerializeField, Header("AUDIO")] private AudioClip _testSFX;
+
+
+    private void Start()
+    {
+        ServiceLocator.Get().PlaySound(_testSFX);
+    }
+
 }
