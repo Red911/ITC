0c7f44a0123b36e3b3735df0b02536235a0f7f34
Romain <mromain911@gmail.com>
2023-10-09 19:04:52 +0200

    [UP]Player Move

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using EasyButtons;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform endingPoint;
-    
-    
 
-    
+    [SerializeField] private float speed = 100f;
+    private bool _isMoving;
+
+    private void FixedUpdate()
+    {
+        print(Vector3.Distance(transform.position, endingPoint.position));
+        if (_isMoving && Vector3.Distance(transform.position, endingPoint.position) > 0.5) 
+        {
+            var dir = new Vector3(endingPoint.position.x - transform.position.x, 0, endingPoint.position.z - transform.position.z);
+            dir.Normalize();
+
+            rb.velocity = dir * (speed * Time.fixedDeltaTime);
+        }
+    }
+
+    [Button]
+    private void Move() => _isMoving = true;
+
+
+
 }
