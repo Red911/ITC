f76833c360c907fafb62d13508924e62d4e63fcb
Romain <mromain911@gmail.com>
2023-10-11 12:06:37 +0200

    [Up] get text in the SO

--- Assets/Script/SO/DialoguesScriptable.cs
+++ Assets/Script/SO/DialoguesScriptable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dialog", menuName = "CreateDialog", order = 0)]
 public class DialoguesScriptable : ScriptableObject
 {
+    public new string name;
     [TextArea]
     public string[] dialogs;
 }
--- Assets/Script/UI/GetTheDialogue.cs
+++ Assets/Script/UI/GetTheDialogue.cs
@@ -1,8 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GetTheDialogue : MonoBehaviour
 {
-    
+    [SerializeField] private DialoguesScriptable _dialoguesSo;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private TextMeshProUGUI _dialogueText;
+
+    private void Start()
+    {
+        _nameText.text = _dialoguesSo.name;
+        _dialogueText.text = _dialoguesSo.dialogs[0];
+        
+    }
 }
