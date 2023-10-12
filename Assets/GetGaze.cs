99ce33a6cfd7c7b43f9540bcdc2ba586655e3221
Romain <mromain911@gmail.com>
2023-10-12 11:45:52 +0200

    [Add] GetGaze

---
+++ Assets/GetGaze.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+using UnityEngine;
+
+public class GetGaze : MonoBehaviour
+{
+    [SerializeField] private GazeAware _gazeAware;
+    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private Material redMat;
+    private Material _startMat;
+
+    private void Start()
+    {
+        _startMat = mesh.material;
+    }
+
+    private void Update()
+    {
+        if (_gazeAware.HasGazeFocus)
+        {
+            mesh.material = redMat;
+        }
+        else
+        {
+            mesh.material = _startMat;
+        }
+    }
+}
