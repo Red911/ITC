44c4793f59f59fd80313ddfa23f6f21552e23c43
Romain <mromain911@gmail.com>
2023-10-13 09:35:09 +0200

    [Up] Get Gaze
    il faut appuyer sur Space et le regarder pour activer l'objet

--- Assets/GetGaze.cs
+++ Assets/GetGaze.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 
 public class GetGaze : MonoBehaviour
 {
+    [Header("Component")]
     [SerializeField] private GazeAware _gazeAware;
     [SerializeField] private MeshRenderer mesh;
     [SerializeField] private Material redMat;
     private Material _startMat;
+
+    [SerializeField]private KeyCode input;
     
 
     private void Start()
@@ -19,6 +22,10 @@ public class GetGaze : MonoBehaviour
 
     private void Update()
     {
-        mesh.material = _gazeAware.HasGazeFocus ? redMat : _startMat;
+        if (_gazeAware.HasGazeFocus && Input.GetKeyDown(input))
+        {
+            mesh.material = redMat;
+        }
+        
     }
 }
